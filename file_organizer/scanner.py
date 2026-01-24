7870700b43dbecbdf580892cbabda47d8bb70582
milinddhawale <milinddhawale05@gmail.com>
2026-01-24 22:03:07 +0530

    add scanner function skeleton with clear contract

---
+++ file_organizer/scanner.py
@@ -0,0 +1,11 @@
+def scan_directory(directory_path):
+    """
+    Scans the given directory and returns metadata about files.
+
+    Args:
+        directory_path (str): Path to the directory to scan
+
+    Returns:
+        list: A list of dictionaries containing file metadata
+    """
+    pass
