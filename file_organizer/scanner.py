90e8a7f8a6d4d2b710a3c7ec41d4b13fcb83c9a2
milinddhawale <milinddhawale05@gmail.com>
2026-01-25 17:37:29 +0530

    add os import for directory scanning

--- file_organizer/scanner.py
+++ file_organizer/scanner.py
@@ -1,11 +1,1 @@
-def scan_directory(directory_path):
-    """
-    Scans the given directory and returns metadata about files.
-
-    Args:
-        directory_path (str): Path to the directory to scan
-
-    Returns:
-        list: A list of dictionaries containing file metadata
-    """
-    pass
+import os
