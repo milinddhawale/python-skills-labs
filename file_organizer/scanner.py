531dbb85341fcdc96a37acb6df9789a87f6eda59
milinddhawale <milinddhawale05@gmail.com>
2026-01-25 17:44:55 +0530

    add scan_directory function skeleton with docstring

--- file_organizer/scanner.py
+++ file_organizer/scanner.py
@@ -1,1 +1,14 @@
 import os
+
+def scan_directory(directory_path):
+    """
+    Scan the given directory and return metadata
+    about files and directories.
+
+    Args:
+        directory_path (str): Path to scan
+
+    Returns:
+        list: List of dictionaries with item metadata
+    """
+    pass
