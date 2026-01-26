9c1254ff75cf13ccac83bbf70101856405abdca5
milinddhawale <milinddhawale05@gmail.com>
2026-01-26 22:55:05 +0530

    iterate over scanned items in organizer

--- file_organizer/organizer.py
+++ file_organizer/organizer.py
@@ -1,12 +1,7 @@
 def decide_targets(scanned_items):
-    """
-    Decide target folders for files based on metadata.
-
-    Args:
-        scanned_items (list): Output from scanner.scan_directory
-
-    Returns:
-        dict: mapping of file path to target folder name
-    """
     targets = {}
+
+    for item in scanned_items:
+        pass
+    
     return targets
