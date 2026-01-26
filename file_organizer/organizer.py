d1377007ac9f31641d494f000dd463fbf06dfcef
milinddhawale <milinddhawale05@gmail.com>
2026-01-26 23:35:01 +0530

    remove temporary manual test from organizer

--- file_organizer/organizer.py
+++ file_organizer/organizer.py
@@ -28,20 +28,3 @@ def decide_targets(scanned_items):
 
     return targets
 
-if __name__ == "__main__":
-    sample = [
-        {"path": "a.pdf", "type": "file", "extension": ".pdf"},
-        {"path": "b.png", "type": "file", "extension": ".png"},
-        {"path": "c", "type": "file", "extension": None},
-        {"path": "folder", "type": "directory", "extension": None}
-    ]
-
-    print(decide_targets(sample))
-
-"""
-output:
-(PSL) C:\Users\milind dhawle\OneDrive\Desktop\python-practice-labs>python file_organizer\organizer.py
-skipping directory: folder
-{'a.pdf': 'documents', 'b.png': 'images', 'c': 'others'}
-
-"""
