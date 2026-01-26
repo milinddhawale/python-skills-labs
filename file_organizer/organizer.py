87a550559954bc4ecf997cf12891d7aca21317bf
milinddhawale <milinddhawale05@gmail.com>
2026-01-26 22:47:21 +0530

    add organizer decide_targets function skeleton

---
+++ file_organizer/organizer.py
@@ -0,0 +1,11 @@
+def decide_targets(scanned_items):
+    """
+    Decide target folders for files based on metadata.
+
+    Args:
+        scanned_items (list): Output from scanner.scan_directory
+
+    Returns:
+        dict: mapping of file path to target folder name
+    """
+    pass
