59a92da354cab34a222f30c58866d3e425adee08
milinddhawale <milinddhawale05@gmail.com>
2026-01-27 22:00:57 +0530

    add move_files function skelton

--- file_organizer/mover.py
+++ file_organizer/mover.py
@@ -1,2 +1,12 @@
 import os
 import shutil
+
+def move_files(target_map, base_directory):
+    """
+    Move files to target folders based on organizer output.
+
+    Args:
+        target_map (dict): mapping of file path to target folder name
+        base_directory (str): directory where target folder will be created.
+    """
+    pass
