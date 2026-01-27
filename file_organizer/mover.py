3255aa1b15e58e0864dc29cf258b0201bdda4250
milinddhawale <milinddhawale05@gmail.com>
2026-01-27 22:16:25 +0530

    create target folders if they do not exist

--- file_organizer/mover.py
+++ file_organizer/mover.py
@@ -9,4 +9,10 @@ def move_files(target_map, base_directory):
         target_map (dict): mapping of file path to target folder name
         base_directory (str): directory where target folder will be created.
     """
-    pass
+    for _, folder in target_map.items():
+        target_dir = os.path.join(base_directory, folder)
+
+        if not os.path.exists(target_dir):
+            os.makedirs(target_dir)
+
+#This ensures - folders exist - no crash on first run
