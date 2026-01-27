c120769157227aebd33417868509753f0ca159c3
milinddhawale <milinddhawale05@gmail.com>
2026-01-27 22:41:52 +0530

    move files to their target directories

--- file_organizer/mover.py
+++ file_organizer/mover.py
@@ -2,17 +2,9 @@ import os
 import shutil
 
 def move_files(target_map, base_directory):
-    """
-    Move files to target folders based on organizer output.
-
-    Args:
-        target_map (dict): mapping of file path to target folder name
-        base_directory (str): directory where target folder will be created.
-    """
-    for _, folder in target_map.items():
+    for file_path, folder in target_map.items():
         target_dir = os.path.join(base_directory, folder)
+        file_name = os.path.basename(file_path)
+        destination = os.path.join(target_dir, file_name)
 
-        if not os.path.exists(target_dir):
-            os.makedirs(target_dir)
-
-#This ensures - folders exist - no crash on first run
+        shutil.move(file_path, destination)
