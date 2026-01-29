9a987e1bbdf9da0116baa2a5686ce10ab3a8ca52
milinddhawale <milinddhawale05@gmail.com>
2026-01-29 20:32:40 +0530

    add error handling and logging to mover

--- file_organizer/mover.py
+++ file_organizer/mover.py
@@ -3,8 +3,13 @@ import shutil
 
 def move_files(target_map, base_directory):
     for file_path, folder in target_map.items():
-        target_dir = os.path.join(base_directory, folder)
-        file_name = os.path.basename(file_path)
-        destination = os.path.join(target_dir, file_name)
+        try:
+           target_dir = os.path.join(base_directory, folder)
+           file_name = os.path.basename(file_path)
+           destination = os.path.join(target_dir, file_name)
 
-        shutil.move(file_path, destination)
+           shutil.move(file_path, destination)
+           print(f"Moved: {file_path} -> {destination}")
+
+        except Exception as e:
+            print(f"Failed to move {file_path}: {e}")
